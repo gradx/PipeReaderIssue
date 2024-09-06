42424fc88d4027b674c29d223c8666d208feaacc
BLUEDEVIL\gradx <gradx@hotmail.com>
2024-09-05 21:36:36 -0400

    Pipeline issue

---
+++ Genie.Web.Api/Program.cs
@@ -0,0 +1,40 @@
+using System.Net;
+
+
+var app = Build(args);
+
+app.MapPost("errors", async (HttpContext httpContext) =>
+{
+    var read = await httpContext.Request.BodyReader.ReadAsync(new CancellationToken());
+    return HttpStatusCode.OK;
+});
+
+app.MapPost("works3", async (HttpContext httpContext) =>
+{
+    var ms = new byte[(int)httpContext.Request.ContentLength];
+    await httpContext.Request.BodyReader.AsStream().ReadAsync(ms, new CancellationToken());
+    return HttpStatusCode.OK;
+});
+
+app.MapPost("works2", async (HttpContext httpContext) =>
+{
+    var ms = new byte[(int)httpContext.Request.ContentLength];
+    var read = await httpContext.Request.Body.ReadAsync(ms, new CancellationToken());
+    return HttpStatusCode.OK;
+});
+
+app.MapPost("works", async (HttpContext httpContext) =>
+{
+    using var ms = new MemoryStream();
+    await httpContext.Request.BodyReader.CopyToAsync(ms, new CancellationToken());
+    return HttpStatusCode.OK;
+});
+
+app.Run();
+
+
+static WebApplication Build(string[] args)
+{
+    var builder = WebApplication.CreateBuilder(args);
+    return builder.Build();
+}
